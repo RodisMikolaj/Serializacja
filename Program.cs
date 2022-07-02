e075a406900acc36b2b9ec2196f309b6ccfbea9a
Mikołaj <mikolaj.sidor@o2.pl>
2022-07-02 21:53:59 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Serializacja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba osoba1 = new Osoba();
+            osoba1.Imie = "Mikołaj";
+            osoba1.Nazwisko = "Sidor";
+            osoba1.Wiek = 21;
+            osoba1.Save("DaneOsobowe.xml");
+            Osoba wczytaj = Osoba.Load("DaneOsobowe.xml");
+        }
+    }
+}
